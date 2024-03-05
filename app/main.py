ab68ac32563c0f6d264c89a1648f016cab30dfbf
christophercarlon <cmcarleone@gmail.com>
2024-03-05 20:16:19 +0000

    Lambda Function SAM Deploy

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 import json
-import os
+import boto3
 from loguru import logger
-from dotenv import load_dotenv
 from data_download import data_url, load_excel_into_memory, upload_to_s3
 
+def get_secrets(secret_name, region_name="eu-west-2"):
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+    
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except Exception as e:
+        logger.error(f"Unable to retrieve secret {secret_name}: {str(e)}")
+        raise e
+    else:
+        # Assuming the secret is stored as a JSON string
+        secret = get_secret_value_response['SecretString']
+        return json.loads(secret)
 
 def main(): 
-    # Your AWS credentials and S3 bucket details
-    load_dotenv()
-    aws_access_key_id = os.getenv('aws_access_key_id')
-    aws_secret_access_key = os.getenv('aws_secret_access_key')
-    bucket_name = os.getenv('bucket_name')
-    object_name = os.getenv('object_name')
+    secrets = get_secrets("leedshmopipecreds")  
+
+    # AWS credentials and S3 bucket details 
+    aws_access_key_id = secrets['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = secrets['AWS_SECRET_ACCESS_KEY']
+    bucket_name = secrets['BUCKET_NAME']
+    object_name = secrets['OBJECT_NAME']
 
     # The URL of the webpage containing the Excel file link
     web_url = "https://datamillnorth.org/dataset/2o13g/houses-in-multiple-occupation-licence-register/"
